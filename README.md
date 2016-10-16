b64922c0d989bb8d9983b8f561b1417e3bd124a1
Declan Hopkins <hopkins.declan@gmail.com>
2016-10-16 12:58:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MyCart-API
+
+This is the backend API for MyCart.
+
+Technology     | Purpose
+---------------|----------
+**Python 2**   | Core
+**Flask**      | API Framework
+**MongoDB**    | Database
+**PyMongo**    | Database Driver
---
+++ mycart.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, abort
+from flask.ext.pymongo import PyMongo
+from bson import json_util
+import json
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route('/api/v1/products', methods=['GET'])
+def get_products():
+    products = list(mongo.db.products.find())
+    return json.dumps(products, sort_keys=True, indent=4, default=json_util.default)
+
+@app.route('/api/v1/products/<string:barcode>', methods=['GET'])
+def get_product(barcode):
+    product = mongo.db.products.find_one({'barcode' : barcode})
+    if product is None:
+	return jsonify({'error': 'Product not found'}), 404
+
+    return json.dumps(product, sort_keys=True, indent=4, default=json_util.default)
+
+@app.route('/api/v1/products/add', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    if not data or 'name' in data:
+        abort(400)
+
+    product = {
+        'name': data['name'],
+        'barcode': data['barcode'],
+        'brand': data['brand'],
+        'price': data['price']
+    }
+    mongo.db.products.insert(product)
+    return jsonify({'product': product}), 201
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
